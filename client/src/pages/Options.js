2344920db4a70f238450ed082a96bb0cc6140adc
Abir Taheer <40152590+abir-taheer@users.noreply.github.com>
2020-01-16 23:20:24 -0500

    added reload for logout

--- client/src/pages/Options.js
+++ client/src/pages/Options.js
@@ -41,6 +41,7 @@ export const Options = props => {
 				if( window.document.cookie.includes("voting_station"))
 					document.location.href = `https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=${window.location.origin}`;
 				context.updateState();
+				window.location.reload();
 			});
 	};
 
